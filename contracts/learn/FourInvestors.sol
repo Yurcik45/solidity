35fb17cac8945a500a7068bc57e0798655b6ea79
yurcik45 <yurcik45mail@gmail.com>
2022-04-25 00:07:59 +0300

    first contract

---
+++ contracts/learn/FourInvestors.sol
@@ -0,0 +1,62 @@
+//SPDX-License-Identifier: UNLICENSED
+
+//pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract FourInvestors {
+    // we have 4 investors
+    // which should donate need sum of money
+    // and ONLY then i can get this money
+    uint64 constant private final_price = 4000000000000000000;
+    address public owner;
+    struct Sender {
+        address account;
+        uint sum;
+        uint timestamp;
+    }
+    Sender[4] public senders;
+    uint8 count;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier checkSum(uint64 _sum) {
+        require(msg.value == _sum, "Incorrect sum");
+        _;
+    }
+    modifier isOwner() {
+        require(msg.sender == owner, "You are not owner");
+        _;
+    }
+    modifier isNotOwner() {
+        require(msg.sender != owner, "You are owner");
+        _;
+    }
+    modifier sumEnought() {
+        require(address(this).balance <= final_price, "Sum is enought");
+        _;
+    }
+    modifier alreadyPayed() {
+       for(uint8 i; i < senders.length; i++) {
+            require(senders[i].account != msg.sender, "You already payed :)");
+        }
+        _;
+    }
+
+    function send() public checkSum(1000000000000000000) isNotOwner alreadyPayed sumEnought payable {
+        if (count == 4) {
+            senders[count] = Sender(msg.sender, msg.value, block.timestamp);
+            count++;
+        } else {
+            count = 0;
+        }
+    }
+    function checkBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    function getMoney(address payable _to) public isOwner sumEnought {
+        _to.transfer(final_price);
+    }
+}
+
